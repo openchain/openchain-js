da74813bd4ffba9300b897666bc1a601a28e4cf2
Flavien Charlon <flavien@charlon.net>
2015-12-28 17:10:13 +0100

    Add unit tests

---
+++ OpenchainJs/tests/apiclient.js
@@ -0,0 +1,15 @@
+﻿var assert = require("assert");
+var openchain = require("../index");
+
+describe('ApiClient', function () {
+    
+    it('getRecord', function () {
+        var client = new openchain.ApiClient("https://test.openchain.org/");
+        return client.getRecord("0000").then(function (result) {
+            assert.equal(result.key, "0000");
+            assert.equal(result.value, "");
+            assert.equal(result.version, "");
+        });
+    });
+
+});
