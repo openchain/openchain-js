c243a4874c367af2d4512593770d7815911ec612
Flavien Charlon <flavien@charlon.net>
2015-12-31 12:35:01 +0100

    Implement the MutationSigner class

--- OpenchainJs/lib/mutationsigner.js
+++ OpenchainJs/lib/mutationsigner.js
@@ -2,8 +2,42 @@
 
 var bitcore = require("bitcore-lib");
 
-var MutationSigner = function (key) {
-    
+/**
+ * Provides the ability to sign a mutation.
+ * 
+ * @constructor
+ * @param {!HDPrivateKey} privateKey The private key used to sign the mutations.
+ */
+var MutationSigner = function (privateKey) {
+    this.privateKey = privateKey;
 };
+
+/** 
+ * Signs a mutatation.
+ * 
+ * @param {!ByteBuffer} mutation The mutation to sign.
+ * @return {!ByteBuffer} The signature.
+ */
+MutationSigner.prototype.sign = function (mutation) {
+    var transactionBuffer = new Uint8Array(mutation.toArrayBuffer());
+    var hash = bitcore.crypto.Hash.sha256(bitcore.crypto.Hash.sha256(transactionBuffer));
+    
+    var signatureBuffer = bitcore.crypto.ECDSA().set({
+        hashbuf: hash,
+        endian: "big",
+        privkey: this.privateKey.privateKey
+    }).sign().sig.toBuffer();
+    
+    return ByteBuffer.wrap(signatureBuffer);
+}
+
+/** 
+ * Get the public key associated to the signer.
+ * 
+ * @return {!ByteBuffer} The public key.
+ */
+MutationSigner.prototype.getPublicKey = function () {
+    return ByteBuffer.wrap(this.privateKey.publicKey.toBuffer());
+}
 
 module.exports = MutationSigner;
