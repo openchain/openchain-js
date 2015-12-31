9ffe9ea63a9a77c36891edf3b78f7a8e1dfa0ea1
Flavien Charlon <flavien@charlon.net>
2015-12-31 13:38:37 +0100

    Document the RecordKey class

--- OpenchainJs/lib/recordkey.js
+++ OpenchainJs/lib/recordkey.js
@@ -1,24 +1,47 @@
 ﻿"use strict";
 
 var ByteBuffer = require("bytebuffer");
-
 var LedgerPath = require("./ledgerpath");
 var encoding = require("./encoding");
 
+/**
+ * Represents the key to a record.
+ * 
+ * @constructor
+ * @param {string} path The path of the record.
+ * @param {string} recordType The type of the record.
+ * @param {string} name The name of the record.
+ */
 var RecordKey = function (path, recordType, name) {
     this.path = LedgerPath.parse(path);
     this.recordType = recordType;
     this.name = name;
 }
 
+/** 
+ * Returns the string representation of the record key.
+ * 
+ * @return {string} The string representation of the record key.
+ */
 RecordKey.prototype.toString = function () {
     return this.path.toString() + ":" + this.recordType + ":" + this.name;
 };
 
+/** 
+ * Returns the binary representation of the record key.
+ * 
+ * @return {!ByteBuffer} The binary representation of the record key.
+ */
 RecordKey.prototype.toByteBuffer = function () {
     return encoding.encodeString(this.toString());
 };
 
+/** 
+ * Parses a record key from a string.
+ * 
+ * @param {string} value The value to parse.
+ * @return {!RecordKey} The parsed record key.
+ */
 RecordKey.parse = function (value) {
     var text = value;
     if (typeof text !== "string") {
