4d50a76f24aa708eaa93752bdde770fdaceae8b9
Flavien Charlon <flavien@charlon.net>
2015-12-31 14:55:26 +0100

    Add the license header to the test files

--- OpenchainJs/tests/ledgerpath.js
+++ OpenchainJs/tests/ledgerpath.js
@@ -1,4 +1,18 @@
-﻿var assert = require("assert");
+﻿// Copyright 2015 Coinprism, Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var assert = require("assert");
 var openchain = require("../index");
 var LedgerPath = openchain.LedgerPath;
 
@@ -8,13 +22,13 @@ describe('LedgerPath', function () {
         assert.equal(new LedgerPath(["a"]).toString(), "/a/");
         assert.equal(new LedgerPath([]).toString(), "/");
     });
-
+    
     it('parse valid', function () {
         assert.deepEqual(LedgerPath.parse("/a/b/c/").parts, ["a", "b", "c"]);
         assert.deepEqual(LedgerPath.parse("/a/").parts, ["a"]);
         assert.deepEqual(LedgerPath.parse("/").parts, []);
     });
-
+    
     it('parse invalid', function () {
         assert.throws(function () { LedgerPath.parse("abc/def/") }, Error);
         assert.throws(function () { LedgerPath.parse("abc/") }, Error);
--- OpenchainJs/tests/schema.js
+++ OpenchainJs/tests/schema.js
@@ -1,4 +1,18 @@
-﻿var assert = require("assert");
+﻿// Copyright 2015 Coinprism, Inc.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+// 
+//     http://www.apache.org/licenses/LICENSE-2.0
+// 
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+var assert = require("assert");
 var openchain = require("../index");
 
 describe("Schema", function () {
@@ -8,12 +22,12 @@ describe("Schema", function () {
         record.key = openchain.Schema.ByteBuffer.fromHex("abcd");
         record.value = openchain.Schema.ByteBuffer.fromHex("0123");
         record.version = openchain.Schema.ByteBuffer.fromHex("4567");
-
+        
         assert.equal(record.key.toHex(), "abcd");
         assert.equal(record.value.toHex(), "0123");
         assert.equal(record.version.toHex(), "4567");
     });
-
+    
     it("Mutation", function () {
         var mutation = new openchain.Schema.Mutation();
         
@@ -21,7 +35,7 @@ describe("Schema", function () {
         
         assert.equal(mutation.namespace.toHex(), "abcd");
     });
-
+    
     it("Transaction", function () {
         var transaction = new openchain.Schema.Transaction();
         
