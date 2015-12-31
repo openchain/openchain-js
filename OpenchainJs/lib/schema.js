f36095598085e65a144ae90f49339946c8aa0e32
Flavien Charlon <flavien@charlon.net>
2015-12-31 11:23:35 +0100

    Correct the formatting of the protocol buffers schema

--- OpenchainJs/lib/schema.js
+++ OpenchainJs/lib/schema.js
@@ -3,30 +3,30 @@
 var ByteBuffer = require("bytebuffer");
 var ProtoBuf = require("protobufjs");
 
-var schema = "\
-syntax = 'proto3'; \
-\
-package Openchain; \
-\
-message RecordValue { \
-    bytes data = 1; \
-} \
-\
-message Record { \
-    bytes key = 1; \
-    RecordValue value = 2; \
-    bytes version = 3; \
-} \
-\
-message Mutation { \
-    bytes namespace = 1; \
-    repeated Record records = 2; \
-    bytes metadata = 3; \
-} \
-\
-message Transaction { \
-    bytes mutation = 1; \
-    int64 timestamp = 2; \
+var schema = "                      \
+syntax = 'proto3';                  \
+                                    \
+package Openchain;                  \
+                                    \
+message RecordValue {               \
+    bytes data = 1;                 \
+}                                   \
+                                    \
+message Record {                    \
+    bytes key = 1;                  \
+    RecordValue value = 2;          \
+    bytes version = 3;              \
+}                                   \
+                                    \
+message Mutation {                  \
+    bytes namespace = 1;            \
+    repeated Record records = 2;    \
+    bytes metadata = 3;             \
+}                                   \
+                                    \
+message Transaction {               \
+    bytes mutation = 1;             \
+    int64 timestamp = 2;            \
     bytes transaction_metadata = 3; \
 }";
 
