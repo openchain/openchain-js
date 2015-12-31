43b5b6ba45ddb0c769632a10e97d4099d88d292c
Flavien Charlon <flavien@charlon.net>
2015-12-31 12:48:38 +0100

    Document the encoding module

--- OpenchainJs/lib/encoding.js
+++ OpenchainJs/lib/encoding.js
@@ -3,17 +3,35 @@
 var ByteBuffer = require("bytebuffer");
 var Long = require("long");
 
+/*
+ * Encodes a string into a byte buffer.
+ * 
+ * @param {string} value The string being encoded.
+ * @return {!ByteBuffer} The encoded string.
+ */
 module.exports.encodeString = function (value) {
     return ByteBuffer.wrap(value, "utf8", true);
 };
 
+/*
+ * Decodes a string from a byte buffer.
+ * 
+ * @param {!ByteBuffer} buffer The buffer containing the string being decoded.
+ * @return {string} The decoded string.
+ */
 module.exports.decodeString = function (buffer) {
     var result = buffer.readUTF8String(buffer.remaining());
     buffer.flip();
     return result;
 };
 
-module.exports.encodeInt64 = function (value, usage) {
+/*
+ * Encodes an integer into a byte buffer.
+ * 
+ * @param {Number|!Long} value The integer being encoded.
+ * @return {!ByteBuffer} The encoded integer.
+ */
+module.exports.encodeInt64 = function (value) {
     var result = new ByteBuffer(null, true);
     result.BE();
     result.writeInt64(value);
@@ -21,6 +39,12 @@ module.exports.encodeInt64 = function (value, usage) {
     return result;
 };
 
+/*
+ * Decodes an integer from a byte buffer.
+ * 
+ * @param {!ByteBuffer} buffer The buffer containing the integer being decoded.
+ * @return {!Long} The decoded string.
+ */
 module.exports.decodeInt64 = function (buffer) {
     if (buffer.limit == 0)
         return Long.ZERO;
